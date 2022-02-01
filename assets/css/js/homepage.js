978018a6abf9c549ab976bf126b8fee1793be5bb
ARod <ajr.rodriguez095@gmail.com>
2022-02-01 01:18:56 -0500

    search by language

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,6 +1,7 @@
   // form reference variables
   var userFormEl = document.querySelector("#user-form");
   var nameInputEl = document.querySelector("#username");
+  var languageButtonsEl = document.querySelector("#language-buttons")
 
   // right column refernece variables
   var repoContainerEl = document.querySelector("#repos-container");
@@ -97,4 +98,31 @@ var getUserRepos = function(user) {
       console.log(searchTerm);
   };
 
+  var getFeaturedRepos = function(language) {
+   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+   fetch(apiUrl).then(function(response) {
+       if (response.ok) {
+           response.json().then(function(data){
+             displayRepos(data.items, language);
+           }); 
+       } else {
+           alert('Error: GitHub User Not Found');
+       }
+   });
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+    console.log(language);
+
+};
+languageButtonsEl.addEventListener("click", buttonClickHandler);
