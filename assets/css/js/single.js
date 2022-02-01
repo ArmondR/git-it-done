35b20b9fbd471648ffadf6b5957918a233606255
ARod <ajr.rodriguez095@gmail.com>
2022-01-31 22:41:38 -0500

    Link Header

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -1,5 +1,6 @@
-// issues container reference
+// container reference
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document. querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,12 +10,17 @@ var getRepoIssues = function(repo) {
           response.json().then(function(data) {
               // pass response data to DOM function
               displayIssues(data);
-              //console.log(data);
+              
+              // check if api has paginated issues
+              if(response.headers.get("Link")) {
+                  displayWarning(repo);
+              }
           });
       } else {
           alert("There was a problem wih your request!");
       }
   });
+  
   //console.log(repo);
 };
 
@@ -53,6 +59,19 @@ var displayIssues = function(issues) {
 
         issuesContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
 
 };
 
